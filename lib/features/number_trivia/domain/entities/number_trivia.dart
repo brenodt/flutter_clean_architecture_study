92190a31479d6558f4d53cd2cbe48736ca46457f
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-12-29 19:36:36 -0300

    Serialize number trivia entity

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class NumberTrivia extends Equatable {
+  NumberTrivia({@required this.text, @required this.number})
+      : super([text, number]);
+
+  final String text;
+  final int number;
+}
